c5ae4b807c1447e6d0efe91ab57c57d700ef9269
riyandifirman <riyandi.firman08@gmail.com>
2023-05-16 23:51:07 +0700

    Completely Navigation Drawer & Snackbar

--- app/src/main/java/com/riyandifirman/mynavdrawer/MainActivity.kt
+++ app/src/main/java/com/riyandifirman/mynavdrawer/MainActivity.kt
@@ -22,22 +22,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyNavDrawerApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyNavDrawerTheme {
-        Greeting("Android")
-    }
-}
